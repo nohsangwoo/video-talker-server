d9d448c96fbeb6d54bbfe5bf1dd8b01b3d69b8a0
nohsangwoo <nsgr12@naver.com>
2021-07-07 17:47:02 +0900

    fix bugs

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ const io = socket(server, {
 io.on('connection', socket => {
   socket.emit('connection', null);
   console.log('new user connected');
-  console.log(socket.io);
+  console.log(socket.id);
 });
