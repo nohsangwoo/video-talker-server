b80b3d01cadf1a9150ba21cfac14a12e8fc977b0
nohsangwoo <nsgr12@naver.com>
2021-07-07 15:31:57 +0900

    connect

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const PORT = 5000;
+
+const app = express();
+
+const server = app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`http://localhost:${PORT}`);
+});
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const io = socket(server, {
+  cors: {
+    origin: '*',
+    methids: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', socket => {
+  socket.emit('connection', null);
+  console.log('new user connected');
+  console.log(socket.io);
+});
